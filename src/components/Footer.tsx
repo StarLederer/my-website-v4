9596b118b0ef1d2872f798ab1c638ff52655669f
Herman Lederer <germans.lederers@gmail.com>
2023-01-18 18:25:44 +0100

    Make footer take children

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,12 @@
-import { Component } from "solid-js";
+import { ParentComponent } from "solid-js";
 import Container from "~/lib/Container";
 import Link from "@/ui/primitives/Button/Link";
 
-const Main: Component = () => {
+const Main: ParentComponent = (props) => {
   return (
     <footer class="bg-def2 text-fg-2 font-semibold">
-      <Container class="pd-m.2">
+      {props.children}
+      <Container class="pd-b-m.2">
         <div class="flex gap-s flex-wrap justify-between items-center">
           <div class="font-bold">Star Lederer</div>
           <Link href="https://github.com/StarLederer"><div class="i-simple-icons-github" /></Link>
