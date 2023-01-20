8686ae528a09c0884a06bdea64969d7b608e61f5
Herman Lederer <germans.lederers@gmail.com>
2023-01-20 17:27:24 +0100

    Add contact to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,8 @@
 import { ParentComponent } from "solid-js";
 import Container from "~/lib/Container";
 import Link from "@/ui/primitives/Button/Link";
+import Button from "@/ui/primitives/Button";
+import navigate from "~/tree/navigate";
 
 const Main: ParentComponent = (props) => {
   return (
@@ -9,7 +11,10 @@ const Main: ParentComponent = (props) => {
       <Container class="pd-b-m.2">
         <div class="flex gap-s flex-wrap justify-between items-center">
           <div class="font-bold">Star Lederer</div>
-          <Link href="https://github.com/StarLederer"><div class="i-simple-icons-github" /></Link>
+          <div class="flex gap-s.4">
+            <Button onClick={() => navigate("/contact")}>Contact</Button>
+            <Link href="https://github.com/StarLederer"><div class="i-simple-icons-github" /></Link>
+          </div>
         </div>
       </Container>
     </footer>
