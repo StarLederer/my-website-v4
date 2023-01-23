80d90be56a788ed4c39126e587edd1641b7ff5f9
Herman Lederer <germans.lederers@gmail.com>
2023-01-23 16:45:11 +0100

    Add unlisted projects for potenial future use

---
+++ src/data/projects/mediacrate.ts
@@ -0,0 +1,11 @@
+import { Project } from "./types";
+import logo from "./logos/placeholder";
+
+const project: Project = {
+    name: "Mediacrate",
+    description: "Not announced yet...",
+    logo: logo,
+    homepageUrl: "mediacrate.xyz",
+};
+
+export default project;
---
+++ src/data/projects/townsAndHeroes.ts
@@ -0,0 +1,12 @@
+import { Project } from "./types";
+import logo from "./logos/placeholder";
+
+const project: Project = {
+  name: "Towns & Heroes",
+  description: "A game about building towns and completing user generated quests",
+  logo: logo,
+  closedSource: true,
+  homepageUrl: "townsandheroes.com",
+};
+
+export default project;
