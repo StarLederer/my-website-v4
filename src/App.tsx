2808a638772da7203cdb13257637f8e427440ab6
Herman Lederer <germans.lederers@gmail.com>
2023-01-31 15:54:13 +0100

    Fix color shemes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import themeStore from "./stores/themeStore";
 import Root from "./tree/Root";
 
 const Main: Component = () => {
-  const themeStyles = () => themeStore.scheme() === "light" ? "theme-light hue-225" : "theme-dark hue-45";
+  const themeStyles = () => themeStore.scheme() === "light" ? "scheme-light hue-225" : "scheme-dark hue-45";
 
   return (
     <main class={`${themeStyles()} size-i-full size-b-full relative bg-def3 text-fg-2 overflow-hidden`}>
