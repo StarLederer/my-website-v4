4e54cc87cb9e6a5afc667fa6c95fe95ef584498d
Herman Lederer <germans.lederers@gmail.com>
2023-02-10 13:20:20 +0100

    Lighten PreFooterCTA colors

--- src/tree/branches/Portfolio/components/PreFooterCTA.tsx
+++ src/tree/branches/Portfolio/components/PreFooterCTA.tsx
@@ -13,10 +13,10 @@ const Main: Component<{
     <Container class="flex items-center justify-center">
       <div class="relative">
         <div class="absolute bg-accent inset-(-m.2) rounded-full transition" style="filter: blur(2rem);" />
-        <div class="relative flex flex-col gap-s justify-center">
+        <div class="relative flex flex-col gap-s justify-center text-fg-3">
           <span class="text-m.2">{props.emoji}</span>
           <span class="font-bold">{props.question}</span>
-          <span class="font-extrabold text-m.2">Get in touch.</span>
+          <span class="font-extrabold text-m.2 text-fg-2">Get in touch.</span>
         </div>
       </div>
     </Container>
